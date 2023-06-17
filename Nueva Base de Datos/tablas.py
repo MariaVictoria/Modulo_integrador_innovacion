ad25f56b6b8bf93598e69947987a170cfcf12187
CarolinaDuarte83 <duarte.carolina.acd@gmail.com>
2023-06-17 15:52:41 -0300

    Subo el UPDATE

--- Nueva Base de Datos/tablas.py
+++ Nueva Base de Datos/tablas.py
@@ -13,14 +13,14 @@ def insert_ingredientes(connection, id_ingredientes, nombre):
     except mysql.connector.Error as err:
         print("Error al insertar ingrediente:", err)
 
-def insert_Productos(connection, idProductos,Nombre, ingredientes,Precio ):
+def insert_Productos(connection, idProductos, Nombre, ingredientes, Precio):
     try:
         cursor = connection.connection.cursor()
-        query = "INSERT INTO Productos (idProductos,Nombre, ingredientes,Precio) VALUES (%s, %s, %s, %s)"
-        values = (idProductos,Nombre, ingredientes,Precio)
+        query = "INSERT INTO Productos (idProductos, Nombre, ingredientes, Precio) VALUES (%s, %s, %s, %s)"
+        values = (idProductos, Nombre, ingredientes, Precio)
         cursor.execute(query, values)
         connection.connection.commit()
-        print("Productos agregado exitosamente.")
+        print("Productos agregados exitosamente.")
     except mysql.connector.Error as err:
         print("Error al insertar Productos:", err)
 
@@ -37,35 +37,22 @@ db_connection = DatabaseConnection(
 db_connection.connect()
 
 # Insertar ingredientes con IDs específicos
-insert_ingredientes(db_connection, 9, "roquefor")
-
+insert_ingredientes(db_connection, 9, "roquefort")
 insert_ingredientes(db_connection, 10, "nuez")
-
 insert_ingredientes(db_connection, 11, "anana")
-
 insert_ingredientes(db_connection, 12, "palmitos")
-
 insert_ingredientes(db_connection, 13, "ternera")
-
 insert_ingredientes(db_connection, 14, "huevo")
 
-
-
-
 # Insertar productos con IDs específicos
-insert_Productos(db_connection, 3, "de verdura", 'tomate, jamon, lechuga, queso','350')
+insert_Productos(db_connection, 3, "de verdura", 'tomate, jamon, lechuga, queso', '350')
+insert_Productos(db_connection, 4, "de roquefort", 'roquefort, nuez, jamon', '500')
+insert_Productos(db_connection, 5, "de rucula", 'jamon crudo, rucula, queso', '350')
+insert_Productos(db_connection, 6, "de anana", 'jamon, anana', '400')
+insert_Productos(db_connection, 7, "de palmitos", 'jamon, palmitos', '400')
+insert_Productos(db_connection, 8, "de ternera y verdura", 'ternera, tomate y huevo', '300')
 
-insert_Productos(db_connection, 4, "de roquefort", 'roquefot, nuez, jamon','500')
-
-insert_Productos(db_connection, 5, "de rucula", 'jamon crudo, rucula, queso','350')
-
-insert_Productos(db_connection, 6, "de anana", 'jamon , anana','400')
-
-insert_Productos(db_connection, 7, "de palmitos", 'jamon , palmitos','400')
-
-insert_Productos(db_connection, 8, "de ternera y verdura", 'ternera , tomate y huevo','300')
-
-#Crear un Select
+# Crear un Select
 cursor = db_connection.connection.cursor()
 cursor.execute("SELECT * FROM productos;")
 resultados = cursor.fetchall()
@@ -73,13 +60,22 @@ resultados = cursor.fetchall()
 for row in resultados:
     print(row[0])
 
-#Crear DELET
-cursor=conexion.cursor()
-consulta="DELETE FROM Pedidos WHERE columna = %s"
-valor=("rodilla",)
+# Crear DELETE
+cursor = db_connection.connection.cursor()
+consulta = "DELETE FROM Pedidos WHERE columna = %s"
+valor = ("rodilla",)
 
-cursor.execute(consulta,valor)
+cursor.execute(consulta, valor)
+
+# Crear UPDATE
+cursor = db_connection.connection.cursor()
+consulta = "UPDATE productos SET columna1 = %s, columna2 = %s WHERE condicion"
+valores = ("de anana", "500")
+cursor.execute(consulta, valores)
+db_connection.connection.commit()
+
 cursor.close()
 
 # Cerrar la conexión a la base de datos
 db_connection.close()
+
